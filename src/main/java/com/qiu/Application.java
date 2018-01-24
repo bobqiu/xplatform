7b807a171f9be6f3683c7a06782da77c9ffdbe8f
cuihailong <cuihailong3ai@gmail.com>
2018-01-24 17:12:26 +0800

    init

---
+++ src/main/java/com/qiu/Application.java
@@ -0,0 +1,17 @@
+package com.qiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Describe:
+ * Created by: bobqiu
+ * Date: 2018/1/24 下午5:10
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
